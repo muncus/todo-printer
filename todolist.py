8751efb62b9408d9cb4e26d8aaba325b339f519c
Marc Dougherty <muncus@gmail.com>
2016-08-03 21:27:06 -0700

    restructure into a class, add markdown output.

--- todolist.py
+++ todolist.py
@@ -5,25 +5,62 @@ import yaml
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Get ToDoist list')
-parser.add_argument('--debug', '-d', help='add debugging output')
-parser.add_argument('--config', '-c', help='yaml config file with token')
-parser.add_argument('--token', '-t', metavar='TOKEN', nargs=1,
-                    help='api token')
 
-args = parser.parse_args()
+class TodoList(object):
+  """Wrap the fetching of todoist todos, and markdown."""    
 
-if args.config:
+  def __init__(self, auth_token=None):
+    self._client = todoist.TodoistAPI(token=auth_token)
+
+  def todoItems(self, query=None):
+    """query syntax is poorly doc'd by todoist api docs."""
+    if not query:
+      query = "viewall"
+    results = {}
+    qresults = self._client.query([query])
+    if not qresults:
+      print "FAIL"
+    for project in qresults[0].get('data', []):
+      results[project['project_name']] = []
+      for todo in project['uncompleted']:
+        results[project['project_name']].append(todo['content'])
+    return results
+
+  def asText(self, query=None):
+    output = ''
+    r = self.todoItems(query)
+    for p in sorted(r.keys()):
+      output += "%s\n" % p
+      for i in r[p]:
+        output += "[ ] %s\n" % i
+    output += "\n"
+    return output
+
+  def asMarkdown(self, query=None):
+    from jinja2 import Template
+    t = Template("""
+{%- for project in projects.keys()|sort %}
+### {{project}}
+{% for i in projects[project] %}
+  * {{i}}
+{%- endfor %}
+{% endfor %}
+""")
+    return t.render(projects=self.todoItems(query))
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Get ToDoist list')
+  parser.add_argument('--config', '-c', help='yaml config file with token')
+  parser.add_argument('--token', '-t', metavar='TOKEN', nargs=1,
+                      help='api token')
+
+  args = parser.parse_args()
+
+  if args.config:
     cfg = yaml.load(file(args.config))
     args.token = cfg['token']
-tdc = todoist.TodoistAPI(token=args.token)
-
-qresults = tdc.query(["viewall"])
-if args.debug:
-    print qresults
-project_list = qresults[0]['data']
-for p in project_list:
-    print p['project_name']
-    for todo in p['uncompleted']:
-        print u"\u2610  %s" % todo['content']
+    l = TodoList(args.token)
+    print l.asText()
+    #print l.asMarkdown()
 
